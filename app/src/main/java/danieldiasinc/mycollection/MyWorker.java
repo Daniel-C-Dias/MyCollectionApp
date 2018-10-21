817cd8da7f179ed2369ac57c032535db818d265d
Daniel-C-Dias <danielcanastro@gmail.com>
2018-10-21 23:15:13 +0100

    Created Android Studio Project

---
+++ app/src/main/java/danieldiasinc/mycollection/MyWorker.java
@@ -0,0 +1,13 @@
+package danieldiasinc.mycollection;
+
+/**
+ * Created by Daniel Dias
+ */
+public class MyWorker
+{
+    public static int doubleTheValue (int value)
+    {
+        return value*2;
+    }
+
+}
